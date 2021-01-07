ce6533e1b298f7bf9e1597d0a1252d356f000e10
Ninitha <67736136+Ninitha06@users.noreply.github.com>
2021-01-07 20:12:13 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,14 +30,12 @@ function setup(){
     createCanvas(400,700);
     umbrella = new Umbrella(200,500);
 
-    //creating drops
-    if(frameCount % 150 === 0){
-
+    
         for(var i=0; i<maxDrops; i++){
             drops.push(new createDrop(random(0,400), random(0,400)));
         }
 
-    }
+    
     
 }
 
